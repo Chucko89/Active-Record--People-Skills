d46d1db70ab6a476054a630e1b8cca3b40644e1f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-05 12:28:40 -0700

    finished

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,9 +1,11 @@
 class Skill < ActiveRecord::Base
   has_many :users, through: :proficiencies
   has_many :proficiencies
-  # validates :name, uniqueness: true
+  validates :name, uniqueness: true
 
-  def proficiency_for(args)
-    skills.find_by_name(args)
+  def user_with_proficiency(number)
+    matched_rating = Proficiency.where(skill_id: self.id, rating: number).first
+    User.find(matched_rating.user_id)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
   has_many :skills, through: :proficiencies
   has_many :proficiencies
+
+  def proficiency_for(skill)
+    user_id = self.id
+    skill_id = skill.id
+    proficiencies_array = Proficiency.where(user_id: user_id, skill_id: skill_id)
+    proficiencies_array.first.rating
+  end
+
+  def set_proficiency_for(skill, number)
+    user_id = self.id
+    skill_id = skill.id
+    proficiencies_array = Proficiency.where(user_id: user_id, skill_id: skill_id)
+    proficiencies_array.first.rating = number
+  end
+
 end
